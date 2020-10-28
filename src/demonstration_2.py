ff9d42bbdad654ef2c7d22fc213600e8278b3f4f
avantgov <alexcouts18@gmail.com>
2020-10-27 23:17:51 -0500

    completed problem 2

--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -56,6 +56,48 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def is_valid_BST(self, root):
-    # Your code here
+def get_height(root):
+    if root is None:
+        return 0
+    else:
+        return 1 + max(get_height(root.left), get_height(root.right))
 
+def balancedBinaryTree(root):
+    if root is None:
+        return True
+    
+    return balancedBinaryTree(root.right) and balancedBinaryTree(root.left) and abs(get_height(root.left) - get_height(root.right)) <= 1
+    # return balancedBinaryTree(root.right) 
+    # and balancedBinaryTree(root.left) 
+    # and abs(get_height(root.left) - get_height(root.right)) <= 1
+
+#  revised solution 
+"""
+REVISED: 
+new helper function will return the height of the tree if balanced, oterwise will return -1 
+- apply the helper function to both sides of the tree root 
+- if either side of the tree is unbalanced then we return -1 
+- if both sub trees return an int value (the height of the sides), then we will check the difference in these heights 
+    - if this difference does not exceed 1, then we will return the height of the tree 
+    - if this difference does exceed 1, then we will return -1 
+
+"""
+def is_balanced_helper(root):
+    if root is None:
+        return 0
+    
+    left_height = is_balanced_helper(root.left)
+    if left_height == -1:
+        return -1
+    
+    right_height = is_balanced_helper(root.right)
+    if right_height == -1:
+        return -1
+    
+    if abs(left_height - right_height) > 1:
+        return -1
+    
+    return max(left_height, right_height) + 1
+
+def balancedBinaryTree(root):
+    return is_balanced_helper(root) > -1
